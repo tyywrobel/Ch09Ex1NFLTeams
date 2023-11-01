058ce5730b201789461c8e9f56bb97219275d7e2
tyywrobel <tylerbeitel25@gmail.com>
2023-10-31 23:09:29 -0400

    Add project files.

---
+++ NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NFLTeams.Models;
+
+namespace NFLTeams.Controllers
+{
+    public class NameController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            var session = new NFLSession(HttpContext.Session);
+            var model = new TeamsViewModel
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv(),
+                Teams = session.GetMyTeams(),
+                Username = session.GetName()
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public RedirectToActionResult Change(TeamsViewModel model)
+        {
+            var session = new NFLSession(HttpContext.Session);
+            session.SetName(model.Username);
+
+            return RedirectToAction("Index", "Home",
+                new {
+                    ActiveConf = session.GetActiveConf(),
+                    ActiveDiv = session.GetActiveDiv()
+                });
+        }
+    }
+}
